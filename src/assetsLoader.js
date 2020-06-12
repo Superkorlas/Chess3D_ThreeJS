0b9e11758679835d120591f70b0a16037b5b8f2c
Superkorlas <superkorlas@gmail.com>
2020-06-12 13:19:26 +0200

    Testing path for github-pages

--- src/assetsLoader.js
+++ src/assetsLoader.js
@@ -4,11 +4,11 @@ var queenModel;
 var bishopModel;
 var knightModel;
 var rookModel;
-var whiteSectionTexture = new THREE.TextureLoader().load("../assets/textures/white_section.jpg");
-var blackSectionTexture = new THREE.TextureLoader().load("../assets/textures/white_section.jpg");
-var whitePieceTexture = new THREE.TextureLoader().load("../assets/textures/white_piece.jpg");
-var blackPieceTexture = new THREE.TextureLoader().load("../assets/textures/black_piece.jpg");
-var piecesModelPath = "../assets/model/"
+var whiteSectionTexture = new THREE.TextureLoader().load("/assets/textures/white_section.jpg");
+var blackSectionTexture = new THREE.TextureLoader().load("/assets/textures/white_section.jpg");
+var whitePieceTexture = new THREE.TextureLoader().load("/assets/textures/white_piece.jpg");
+var blackPieceTexture = new THREE.TextureLoader().load("/assets/textures/black_piece.jpg");
+var piecesModelPath = "/assets/model/";
 
 function getModel(name) {
 	switch(name) {
